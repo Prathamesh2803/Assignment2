cfe741ac0b5c15fcdc38e4ea2a59879224b2d74b
Prathamesh2803 <68282464+Prathamesh2803@users.noreply.github.com>
2020-07-17 21:12:19 +0530

    Update Inverted_Pyramid.java

--- Inverted_Pyramid.java
+++ Inverted_Pyramid.java
@@ -3,31 +3,24 @@ import java.util.Scanner;
 public class Inverted_Pyramid {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		// Taking noOfRows value from the user
-
-		System.out.println("No. of rows: ");
-
+	        System.out.println("No. of rows: ");
 		int n = sc.nextInt();
         int rowsCount=n;
 		sc.close();
-
-		// Implementing the logic
-
 		for (int i = 0; i < n; i++) {
-			// Printing i*2 spaces at the beginning of each row
+		
 
 			for (int j = 1; j <= i * 2; j++) {
 				System.out.print(" ");
 			}
 
-			// Printing j where j value will be from 1 to rowCount
+			
 
 			for (int j = 1; j <= rowsCount; j++) {
 				System.out.print(j + " ");
 			}
 
-			// Printing j where j value will be from rowCount-1 to 1
+			
 
 			for (int j = rowsCount - 1; j >= 1; j--) {
 				System.out.print(j + " ");
@@ -35,7 +28,7 @@ public class Inverted_Pyramid {
 
 			System.out.println();
 
-			// Decrementing the rowCount
+		
 
 			rowsCount--;
 		}
